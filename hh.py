38472c2a807a3c01bd329c82b57a24070b4fc4c0
Vladimir Polyakov <vlpolyakov@mail.ru>
2023-04-28 04:28:44 +0400

    Закомментил ненужные методы в hh.py. Потом нужно будет их вообще удалить.

--- hh.py
+++ hh.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 HH_URL = 'https://api.hh.ru/'
-JOBS_LIST_FILE = 'settings/jobs_ex.csv'
+# JOBS_LIST_FILE = 'settings/jobs_ex.csv'
 
 
 class HH:
@@ -15,11 +15,12 @@ class HH:
             per_page=100,
             search_field='name',
             area=79,
-            jobs_list_file=JOBS_LIST_FILE,):
+            # jobs_list_file=JOBS_LIST_FILE,
+    ):
 
         self.search_period = search_period
         self.per_page = per_page
-        self.jobs_list_file = jobs_list_file
+        # self.jobs_list_file = jobs_list_file
         self.search_field = search_field
         self.area = area
         self.col_dtypes = {
@@ -36,20 +37,20 @@ class HH:
             'published_at': 'datetime64[ns]',
         }
 
-    def read_vacancies_titles_from_csv(self):
-        """
-        Чтение csv файла с названиями вакансий
-        """
-        vacancies = pd.read_csv(self.jobs_list_file)
-        vacancies = vacancies[vacancies['search'].notna()]
-        vacancy_titles = vacancies['title'].to_list()
-
-        print('Будет произведен анализ следующих вакансий:')
-        for title in vacancy_titles:
-            print(title)
-        print("")
-
-        return vacancy_titles
+    # def read_vacancies_titles_from_csv(self):
+    #     """
+    #     Чтение csv файла с названиями вакансий
+    #     """
+    #     vacancies = pd.read_csv(self.jobs_list_file)
+    #     vacancies = vacancies[vacancies['search'].notna()]
+    #     vacancy_titles = vacancies['title'].to_list()
+    #
+    #     print('Будет произведен анализ следующих вакансий:')
+    #     for title in vacancy_titles:
+    #         print(title)
+    #     print("")
+    #
+    #     return vacancy_titles
 
     def parse_hh_page(self, job, page=0):
         payloads = {
@@ -64,9 +65,9 @@ class HH:
         }
         return requests.get(f'{HH_URL}vacancies', params=payloads).json()
 
-    def get_vacancies_hh_summary(self, job: str) -> str:
-        r = self.parse_hh_page(job)
-        return f'Поиск по слову: "{job}". Найдено вакансий: {r["found"]}'
+    # def get_vacancies_hh_summary(self, job: str) -> str:
+    #     r = self.parse_hh_page(job)
+    #     return f'Поиск по слову: "{job}". Найдено вакансий: {r["found"]}'
 
     def parse_vacancies(self, job: str):
         vacancies = {col: [] for col in self.col_dtypes.keys()}
@@ -96,14 +97,15 @@ class HH:
 
         return vacancies
 
-    def aprise(self, row):
+    @staticmethod
+    def aprise(row):
         if row.loc['salary_to'] is pd.NA:
             return int(round(row.loc['salary_from'] * 1.1, 0))
         if row.loc['salary_from'] is pd.NA:
             return int(round(row.loc['salary_to'] * .9, 0))
         return int(round((row.loc['salary_to'] + row.loc['salary_from']) / 2, 0))
 
-    def get_vacancies(self, job='1с программист') -> pd.DataFrame:
+    def get_vacancies(self, job) -> pd.DataFrame:
         vacs_data = self.parse_vacancies(job)
         vacancy_df = pd.DataFrame(vacs_data)
         if vacancy_df.empty:
@@ -119,18 +121,18 @@ class HH:
         print(job, vacancy_df.shape[0])
         return vacancy_df
 
-    def get_vacancies_all_jobs(self) -> pd.DataFrame:
-        all_vacancies_df_list = []
-        for job in self.read_vacancies_titles_from_csv():
-            all_vacancies_df_list.append(self.get_vacancies(job))
-        all_vacancies_df = pd.concat(all_vacancies_df_list, ignore_index=True)
-        all_vacancies_df.sort_values(by=['name', 'employer_name', 'published_at'], ascending=False, inplace=True)
-        all_vacancies_df.drop_duplicates(subset=['name', 'employer_id'], keep='first', inplace=True)
-        all_vacancies_df.reset_index(drop=True, inplace=True)
-        return all_vacancies_df
+    # def get_vacancies_all_jobs(self) -> pd.DataFrame:
+    #     all_vacancies_df_list = []
+    #     for job in self.read_vacancies_titles_from_csv():
+    #         all_vacancies_df_list.append(self.get_vacancies(job))
+    #     all_vacancies_df = pd.concat(all_vacancies_df_list, ignore_index=True)
+    #     all_vacancies_df.sort_values(by=['name', 'employer_name', 'published_at'], ascending=False, inplace=True)
+    #     all_vacancies_df.drop_duplicates(subset=['name', 'employer_id'], keep='first', inplace=True)
+    #     all_vacancies_df.reset_index(drop=True, inplace=True)
+    #     return all_vacancies_df
 
 
 if __name__ == '__main__':
     hh = HH()
-    df = hh.get_vacancies()
+    df = hh.get_vacancies('1с программист')
     df.info()
